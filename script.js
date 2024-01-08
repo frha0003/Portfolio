f3701d9f5782e2327fa2f6cf415e112fd81eb9eb
Freja <frha0003@stud.kea.dk>
2024-01-08 14:29:51 +0100

    8/1

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-function rotateStjerne() {
-  console.log("Function called");
-  const stjerne = document.getElementById("Stjerne");
-  stjerne.style.transition = "transform 1s ease-in-out";
-  stjerne.style.transform = "rotate(360deg)";
-}
+document.addEventListener("DOMContentLoaded", function () {
+  const menuBtn = document.querySelector(".menu-btn");
+  const menu = document.querySelector(".menu"); // Brug ".menu" i stedet for "menu"
+
+  menuBtn.addEventListener("click", function () {
+    menu.classList.toggle("active");
+  });
+});
